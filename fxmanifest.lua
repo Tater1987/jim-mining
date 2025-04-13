73e8bbbef90f7c5068f5aea6977c46982c67b534
Tater1987 <149222217+Tater1987@users.noreply.github.com>
2025-04-12 19:24:46 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,8 +5,23 @@ description "Mining Script By Jimathy"
 fx_version "cerulean"
 game "gta5"
 
-shared_scripts { 'config.lua', 'shared/*.lua', 'locales/*.lua' }
-server_script { 'server.lua' }
-client_scripts { 'client.lua' }
+shared_scripts {
+  'config.lua',
+  'shared/*.lua',
+  'locales/*.lua'
+
+    --Jim Bridge
+  '@jim_bridge/exports.lua',
+  '@jim_bridge/functions.lua',
+  '@jim_bridge/wrapper.lua',
+  '@jim_bridge/crafting.lua',
+	'shared/*.lua',
+}
+server_script { 
+  'server.lua'
+}
+client_scripts { 
+  'client.lua'
+}
 
 lua54 'yes'
